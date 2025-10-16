08de8566fd3dd8b4f4b6b9a304d291e1ad38c501
CorentinVyn <167628431+CorentinVyn@users.noreply.github.com>
2025-10-16 13:00:18 +0200

    ajout fonction division

--- Calculatrice/calculatrice.js
+++ Calculatrice/calculatrice.js
@@ -1,6 +1,10 @@
-premier_chiffre = getElementById('premier_chiffre')
-deuxieme_chiffre = getElementById('deuxieme_chiffre')
+// Sélection des éléments HTML
+const premierChiffre = document.getElementById("premier_chiffre");
+const deuxiemeChiffre = document.getElementById("deuxieme_chiffre");
+const resultat = document.getElementById("resultat");
 
-function division(){
-    
+
+function division(premier_chiffre,deuxieme_chiffre){
+    const division=  premier_chiffre/deuxieme_chiffre;
+    resultat.textContent = `Résultat : ${division}`;
 }
